6257991da413aec291dba3b5bdc7f6800ba8a3c4
Nikita Biryuchkov <55081007+biryuc@users.noreply.github.com>
2022-03-11 18:38:16 +0700

    Add files via upload

---
+++ assignment2/metrics.py
@@ -0,0 +1,21 @@
+def multiclass_accuracy(prediction, ground_truth):
+    """
+    Computes metrics for multiclass classification
+
+    Arguments:
+    prediction, np array of int (num_samples) - model predictions
+    ground_truth, np array of int (num_samples) - true labels
+
+    Returns:
+    accuracy - ratio of accurate predictions to total samples
+    """
+    t=0
+    f=0
+    
+    for i in range(len(prediction)):        
+        if prediction[i] == ground_truth[i] :
+            t+=1
+        else:
+            f+=1
+    
+    return t/(t+f)
