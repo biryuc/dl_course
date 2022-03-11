d19ec0102966d29c321af196df2ac1f8918fc501
Nikita Biryuchkov <55081007+biryuc@users.noreply.github.com>
2022-03-11 18:37:02 +0700

    Create layers.py

---
+++ assignment2/layers.py
@@ -0,0 +1,145 @@
+import numpy as np
+
+def softmax(predictions):
+    predictions_copy = np.copy(predictions)
+    if (predictions_copy.ndim == 1):
+        predictions_copy -= np.max(predictions_copy)
+        exp_pred = np.exp(predictions_copy)
+        exp_sum = np.sum(exp_pred)
+        probs = exp_pred / exp_sum
+    else:
+        predictions_copy = (predictions_copy.T - np.max(predictions_copy, axis = 1)).T
+        exp_pred = np.exp(predictions_copy)
+        exp_sum = np.sum(exp_pred, axis = 1)
+        probs = (exp_pred.T / exp_sum).T
+        
+    return probs
+
+
+def cross_entropy_loss(probs, target_index):
+    if (probs.ndim == 1):
+        loss = -np.log(probs[target_index])
+    else:
+        batch_size = probs.shape[0]
+        loss_arr = -np.log(probs[range(batch_size), target_index])
+        loss = np.sum(loss_arr) / batch_size
+        
+    return loss
+
+def l2_regularization(W, reg_strength):
+    """
+    Computes L2 regularization loss on weights and its gradient
+
+    Arguments:
+      W, np array - weights
+      reg_strength - float value
+
+    Returns:
+      loss, single value - l2 regularization loss
+      gradient, np.array same shape as W - gradient of weight by l2 loss
+    """
+    loss = reg_strength * np.sum(np.square(W))
+    grad = 2 * reg_strength * W
+
+    return loss, grad
+
+
+def softmax_with_cross_entropy(predictions, target_index):
+    """
+    Computes softmax and cross-entropy loss for model predictions,
+    including the gradient
+
+    Arguments:
+      predictions, np array, shape is either (N) or (batch_size, N) -
+        classifier output
+      target_index: np array of int, shape is (1) or (batch_size) -
+        index of the true class for given sample(s)
+
+    Returns:
+      loss, single value - cross-entropy loss
+      dprediction, np array same shape as predictions - gradient of predictions by loss value
+    """
+    probs = softmax(predictions)
+    loss = cross_entropy_loss(probs, target_index)
+    dprediction = probs
+    if (probs.ndim == 1):
+        dprediction[target_index] -= 1
+    else:
+        batch_size = predictions.shape[0]
+        dprediction[range(batch_size), target_index] -= 1
+        dprediction /= batch_size
+
+    return loss, dprediction
+
+
+class Param:
+    """
+    Trainable parameter of the model
+    Captures both parameter value and the gradient
+    """
+
+    def __init__(self, value):
+        self.value = value
+        self.grad = np.zeros_like(value)
+
+
+class ReLULayer:
+    def __init__(self):
+        pass
+
+    def forward(self, X):
+        self.X = X
+        return np.clip(X,0,None)
+
+    def backward(self, d_out):
+        """
+        Backward pass
+
+        Arguments:
+        d_out, np array (batch_size, num_features) - gradient
+           of loss function with respect to output
+
+        Returns:
+        d_result: np array (batch_size, num_features) - gradient
+          with respect to input
+        """
+        d_result = (self.X>0)*d_out
+        return d_result
+
+    def params(self):
+        # ReLU Doesn't have any parameters
+        return {}
+
+
+class FullyConnectedLayer:
+    def __init__(self, n_input, n_output):
+        self.W = Param(0.001 * np.random.randn(n_input, n_output))
+        self.B = Param(0.001 * np.random.randn(1, n_output))
+        self.X = None
+
+    def forward(self, X):
+        self.X = X
+        return np.dot(X, self.W.value) + self.B.value
+
+    def backward(self, d_out):
+        """
+        Backward pass
+        Computes gradient with respect to input and
+        accumulates gradients within self.W and self.B
+
+        Arguments:
+        d_out, np array (batch_size, n_output) - gradient
+           of loss function with respect to output
+
+        Returns:
+        d_result: np array (batch_size, n_input) - gradient
+          with respect to input
+        """
+        self.W.grad = np.dot(self.X.T, d_out)
+        self.B.grad = np.sum(d_out, axis = 0, keepdims = True)
+        d_result = np.dot(d_out, self.W.value.T)
+
+        return d_result
+
+    def params(self):
+        return {'W': self.W, 'B': self.B}
